4d0c0f46892ab1f73eb36280fba89cbf0a729e33
Ezekiel <iamwebwiz@gmail.com>
2019-02-01 03:03:42 +0100

    update files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,12 @@
-require('./bootstrap');
+require('./bootstrap')
 
-window.Vue = require('vue');
+window.Vue = require('vue')
 
-Vue.config.productionTip = false;
+import router from './routes'
 
-Vue.component('Task', require('./components/Task.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.config.productionTip = false
 
 const app = new Vue({
     el: '#app',
-    component: 'Task'
+    router
 });
